457096c364de352334937e1fec222f226bea01a8
Sachini <nethranjalise@gmail.com>
2026-05-17 10:53:27 +0530

    update frontend configuration

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // frontend/config.js
-export const API_URL = "http://localhost:5000/api/jobs";
-export const AUTH_URL = "http://localhost:5000/api/auth/login";
+export const API_URL =process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/jobs";
+export const AUTH_URL =process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:5000/api/auth/login";
